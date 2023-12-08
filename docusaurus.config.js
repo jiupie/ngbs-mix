d3a58f2c2f7421d37c51fef2eaea0c8adbabd454
ngbs <17674641491@163.com>
2023-12-08 14:50:22 +0800

    update url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,15 +4,15 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula')
 // With JSDoc @type annotations, IDEs can provide config autocompletion
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  title: '南顾北衫-mix',
+  tagline: '南顾北衫-mix',
+  url: '-',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: '-', // Usually your GitHub org/user name.
+  projectName: 'ngbs-mix', // Usually your repo name.
 
   presets: [
     [
@@ -22,13 +22,12 @@ module.exports = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/'
+          editUrl: 'https://github.com/jiupie/ngbs-mix'
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/main/website/blog/'
+          editUrl: 'https://github.com/jiupie/ngbs-mix/blog/'
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css')
